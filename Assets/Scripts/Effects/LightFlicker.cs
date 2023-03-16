bc8be44123dd7921a596fca79e85e414cc1e7000
Your Name <BenArmour72@gmail.com>
2023-03-16 00:47:35 -0400

    Updated LightFlicker script
    - Removed unused 'using System.Collections.Generic;'
    - Adjusted formatting for better readability and consistency

--- Assets/Scripts/Effects/LightFlicker.cs
+++ Assets/Scripts/Effects/LightFlicker.cs
@@ -1,32 +1,35 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class LightFlicker : MonoBehaviour {
-
-    [Range(0,10)]
+public class LightFlicker : MonoBehaviour
+{
+    [Range(0, 10)]
     public float lightIntensity = 1;
-    [Range(0,10)]
+    [Range(0, 10)]
     public float flickerIntensity = 2;
-    [Range(0,10)]
+    [Range(0, 10)]
     public float lightDuration = 1;
-    [Range(0,10)]
+    [Range(0, 10)]
     public float flickerDuration = 0.2f;
 
-    System.Random randomGenerator;
-    Light flashlight;
+    private System.Random randomGenerator;
+    private Light flashlight;
 
-    void Awake() {
+    private void Awake()
+    {
         randomGenerator = new System.Random();
         flashlight = GetComponent<Light>();
     }
 
-    void Start() {
+    private void Start()
+    {
         StartCoroutine(Flicker());
     }
 
-    IEnumerator Flicker() {
-        while(true) {
+    private IEnumerator Flicker()
+    {
+        while (true)
+        {
             flashlight.intensity = lightIntensity;
 
             float lightingTime = lightDuration + (float)(randomGenerator.NextDouble() - 0.5f) * lightDuration;
@@ -34,7 +37,8 @@ public class LightFlicker : MonoBehaviour {
 
             int flickerCount = randomGenerator.Next(4, 9);
 
-            for(int i = 0; i < flickerCount; i++) {
+            for (int i = 0; i < flickerCount; i++)
+            {
                 float flickingIntensity = lightIntensity - (float)randomGenerator.NextDouble() * flickerIntensity;
                 flashlight.intensity = flickingIntensity;
 
